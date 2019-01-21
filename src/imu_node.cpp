174a604d1ff6c805f8f1f8bcaab257353ae7de36
Daniel Hammer <braineniac@protonmail.com>
2019-01-21 14:00:45 +0100

    imu_node cppified

--- src/imu_node.cpp
+++ src/imu_node.cpp
@@ -1,105 +1,117 @@
+#include <vector>
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <sensor_msgs/Imu.h>
 #include <geometry_msgs/TwistStamped.h>
 
-#include <sstream>
+class ImuNode {
+    public:
+        ImuNode();
 
-char output[50];
-sensor_msgs::Imu imu_old;
-sensor_msgs::Imu imu_new;
-geometry_msgs::TwistStamped pos_new;
-double imu_lin_offset_x=0;
-double imu_lin_offset_y=0;
-double imu_lin_offset_z=0;
-double imu_ang_offset_x=0;
-double imu_ang_offset_y=0;
-double imu_ang_offset_z=0;
-int imu_cb_count=0;
+    private:
+        ros::NodeHandle nh;
+        ros::Publisher pos_pub;
+		ros::Subscriber imu_sub;
 
+        void imu_cb(const sensor_msgs::Imu::ConstPtr& msg);
+        void update_position();
+        void publish_position();
 
-void imu_cb(const sensor_msgs::Imu::ConstPtr& msg) {
+        sensor_msgs::Imu imu_old;
+        sensor_msgs::Imu imu_new;
+        int imu_cb_count;
 
-				imu_new.header = msg->header;
-				imu_new.linear_acceleration = msg->linear_acceleration;
-				imu_new.angular_velocity = msg->angular_velocity;
-				imu_cb_count++;
-				if(imu_cb_count == 5) {
-								imu_lin_offset_x = msg->linear_acceleration.x;
-								imu_lin_offset_y = msg->linear_acceleration.y;
-								imu_lin_offset_z = msg->linear_acceleration.z;
-								imu_ang_offset_x = msg->angular_velocity.x;
-								imu_ang_offset_y = msg->angular_velocity.y;
-								imu_ang_offset_z = msg->angular_velocity.z;
+        std::vector<double> linear_position  = std::vector<double>(3);
+        std::vector<double> angular_position = std::vector<double>(3);
+        std::vector<double> linear_offset    = std::vector<double>(3);
+        std::vector<double> angular_offset   = std::vector<double>(3);
+        std::vector<double> angular_velocity = std::vector<double>(3);
+};
 
-								snprintf(output,50,"linear offset x=%f", imu_lin_offset_x);
-								ROS_INFO(output);
-								snprintf(output,50,"linear offset y=%f", imu_lin_offset_y);
-								ROS_INFO(output);
-								snprintf(output,50,"linear offset z=%f", imu_lin_offset_z);
-								ROS_INFO(output);
-								snprintf(output,50,"angular offset x=%f", imu_ang_offset_x);
-								ROS_INFO(output);
-								snprintf(output,50,"angular offset y=%f", imu_ang_offset_y);
-								ROS_INFO(output);
-								snprintf(output,50,"angular offset z=%f", imu_ang_offset_z);
-								ROS_INFO(output);
+ImuNode::ImuNode()
+{
+    pos_pub = nh.advertise<geometry_msgs::TwistStamped>("imu_pos", 1);
+    imu_sub = nh.subscribe("/imu/data", 10, &ImuNode::imu_cb, this);
+    imu_cb_count = 0;
+}
 
+void ImuNode::imu_cb(const sensor_msgs::Imu::ConstPtr& msg) {
 
+	ImuNode::imu_new.header = msg->header;
+	ImuNode::imu_new.linear_acceleration = msg->linear_acceleration;
+	ImuNode::imu_new.angular_velocity = msg->angular_velocity;
+	ImuNode::imu_cb_count++;
 
-				}
+    if(ImuNode::imu_cb_count == 5) {
+
+        ImuNode::linear_offset[0] = msg->linear_acceleration.x;
+		ImuNode::linear_offset[1] = msg->linear_acceleration.y;
+		ImuNode::linear_offset[2] = msg->linear_acceleration.z;
+		ImuNode::angular_offset[0] = msg->angular_velocity.x;
+		ImuNode::angular_offset[1] = msg->angular_velocity.y;
+		ImuNode::angular_offset[2] = msg->angular_velocity.z;
+    }
+    else if(ImuNode::imu_cb_count > 6) {
+        ImuNode::update_position();
+    }
 
 }
 
+void ImuNode::update_position() {
+
+    double new_t = ImuNode::imu_new.header.stamp.sec + ImuNode::imu_new.header.stamp.nsec/1000000000.0;
+    double old_t = ImuNode::imu_old.header.stamp.sec + ImuNode::imu_old.header.stamp.nsec/1000000000.0;
+    double delta_t = new_t - old_t;
+
+    std::vector<double> delta_a = std::vector<double>(3);
+	delta_a[0] = ImuNode::imu_new.linear_acceleration.x - ImuNode::imu_old.linear_acceleration.x - ImuNode::linear_offset[0];
+	delta_a[1] = ImuNode::imu_new.linear_acceleration.y - ImuNode::imu_old.linear_acceleration.x - ImuNode::linear_offset[1];
+	delta_a[2] = ImuNode::imu_new.linear_acceleration.z - ImuNode::imu_old.linear_acceleration.x - ImuNode::linear_offset[2];
+
+	std::vector<double> linear_path = std::vector<double>(3);
+    linear_path[0] = (ImuNode::imu_old.linear_acceleration.x-ImuNode::linear_offset[0])*delta_t;
+    linear_path[1] = (ImuNode::imu_old.linear_acceleration.y-ImuNode::linear_offset[1])*delta_t;
+    linear_path[2] = (ImuNode::imu_old.linear_acceleration.z-ImuNode::linear_offset[2])*delta_t;
+
+	ImuNode::linear_position[0] =  0.5*delta_a[0]*delta_t*delta_t + linear_path[0];
+	ImuNode::linear_position[1] =  0.5*delta_a[1]*delta_t*delta_t + linear_path[1];
+	ImuNode::linear_position[2] =  0.5*delta_a[2]*delta_t*delta_t + linear_path[2];
+
+	ImuNode::angular_velocity[0] = (ImuNode::imu_old.angular_velocity.x - ImuNode::angular_offset[0]) * delta_t;
+	ImuNode::angular_velocity[1] = (ImuNode::imu_old.angular_velocity.y - ImuNode::angular_offset[1]) * delta_t;
+	ImuNode::angular_velocity[2] = (ImuNode::imu_old.angular_velocity.z - ImuNode::angular_offset[2]) * delta_t;
+
+	ImuNode::publish_position();
+}
+
+void ImuNode::publish_position() {
+
+    // sanity check
+	if (ImuNode::linear_position[0] > 0.03 || ImuNode::linear_position[1] > 0.03)
+        if (ImuNode::linear_position[0] < 0.5 || ImuNode::linear_position[2] < 0.5) {
+
+            geometry_msgs::TwistStamped twist_stamped;
+            twist_stamped.header = ImuNode::imu_old.header;
+            twist_stamped.twist.linear.x = ImuNode::linear_position[0];
+            twist_stamped.twist.linear.y = ImuNode::linear_position[1];
+            twist_stamped.twist.linear.z = ImuNode::linear_position[2];
+            twist_stamped.twist.angular.x = ImuNode::angular_velocity[0];
+            twist_stamped.twist.angular.y = ImuNode::angular_velocity[1];
+            twist_stamped.twist.angular.z = ImuNode::angular_velocity[2];
+
+            ImuNode::pos_pub.publish(twist_stamped);
+        }
+
+    ImuNode::imu_old = ImuNode::imu_new;
+
+}
 
 int main(int argc, char **argv) {
 
-				ros::init(argc, argv, "imu_node");
-				ros::NodeHandle n;
+	ros::init(argc, argv, "imu_node");
+	ImuNode imu_node;
 
-				ros::Publisher pos_pub = n.advertise<geometry_msgs::TwistStamped>("imu_pos", 100);
-				ros::Subscriber imu_sub = n.subscribe("/imu/data", 1000, imu_cb);
-
-				ros::Rate loop_rate(10);
-
-				while(ros::ok()) {
-								
-								double new_t = imu_new.header.stamp.sec + imu_new.header.stamp.nsec/1000000000.0;
-								double old_t = imu_old.header.stamp.sec + imu_old.header.stamp.nsec/1000000000.0;
-								double delta_t = new_t - old_t;
-								snprintf(output,50,"%f",delta_t);
-								ROS_INFO(output);
-
-								pos_new.header = imu_new.header;
-								double delta_a_x = imu_new.linear_acceleration.x - imu_old.linear_acceleration.x - imu_lin_offset_x;
-								double delta_a_y = imu_new.linear_acceleration.y - imu_old.linear_acceleration.x - imu_lin_offset_y;
-								double delta_a_z = imu_new.linear_acceleration.z - imu_old.linear_acceleration.x - imu_lin_offset_z;
-								
-								double new_pos_x =  0.5*delta_a_x*delta_t*delta_t + (imu_old.linear_acceleration.x-imu_lin_offset_x)*delta_t;
-								double new_pos_y =  0.5*delta_a_y*delta_t*delta_t + (imu_old.linear_acceleration.y-imu_lin_offset_y)*delta_t;
-								double new_pos_z =  0.5*delta_a_z*delta_t*delta_t + (imu_old.linear_acceleration.z-imu_lin_offset_z)*delta_t;
-
-
-			  				pos_new.twist.linear.x = new_pos_x;			  					  	  				
-								pos_new.twist.linear.y = new_pos_y;
-								pos_new.twist.linear.z = new_pos_z;
-								
-								pos_new.twist.angular.x = (imu_old.angular_velocity.x - imu_ang_offset_x) * delta_t;
-								pos_new.twist.angular.y = (imu_old.angular_velocity.y - imu_ang_offset_y) * delta_t;
-								pos_new.twist.angular.z = (imu_old.angular_velocity.z - imu_ang_offset_z) * delta_t;
-	
-								imu_old.header = imu_new.header;
-								imu_old.linear_acceleration = imu_new.linear_acceleration;
-								imu_old.angular_velocity = imu_new.angular_velocity;
-								
-								if ((new_pos_x > 0.03 || new_pos_y > 0.03) && (new_pos_x<1.0 || new_pos_y < 1.0))
-										pos_pub.publish(pos_new);
-
-								ros::spinOnce();
-								loop_rate.sleep();
-
-				}
-
-				return 0;
+    ros::spin();
 
 }
