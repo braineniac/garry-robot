17ff9226a87ee6f43299b8e720930411bd5e14a7
Daniel Hammer <braineniac@protonmail.com>
2019-01-23 01:34:50 +0100

    Added state publidher to launch files

--- src/teleop_node.cpp
+++ src/teleop_node.cpp
@@ -5,7 +5,7 @@
 class TeleopGarry {
 	public:
 		TeleopGarry();
-	
+
 	private:
 		void joy_cb(const sensor_msgs::Joy::ConstPtr& joy);
 
@@ -27,7 +27,7 @@ TeleopGarry::TeleopGarry():
 	nh_.param("scale_angular", a_scale_, a_scale_);
 	nh_.param("scale_linear", l_scale_, l_scale_);
 
-	vel_pub_ = nh_.advertise<geometry_msgs::Twist>("garry/cmd_vel", 1);
+	vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/arduino/motor_both", 1);
 
 	joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopGarry::joy_cb, this);
 
