db3accd6dcf5082fb6d89d541fd40c79811935f1
Daniel Hammer <braineniac@protonmail.com>
2019-01-24 22:58:02 +0100

    teleop working correctly

--- src/teleop_node.cpp
+++ src/teleop_node.cpp
@@ -2,50 +2,47 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
-class TeleopGarry {
-	public:
-		TeleopGarry();
+class TeleopNode {
+    public:
+        TeleopNode();
+    private:
+        ros::NodeHandle nh;
 
-	private:
-		void joy_cb(const sensor_msgs::Joy::ConstPtr& joy);
+        ros::Publisher vel_pub;
+        ros::Subscriber joy_sub;
 
-		ros::NodeHandle nh_;
+        void joy_cb(const sensor_msgs::Joy::ConstPtr& joy);
 
-		int linear_, angular_;
-		double l_scale_, a_scale_;
-		ros::Publisher vel_pub_;
-		ros::Subscriber joy_sub_;
+        int linear, angular;
+        double l_scale, a_scale;
 
 };
 
-TeleopGarry::TeleopGarry():
-	linear_(1),
-	angular_(2)
-{
-	nh_.param("axis_linear", linear_, linear_);
-	nh_.param("axis_angular", angular_, angular_);
-	nh_.param("scale_angular", a_scale_, a_scale_);
-	nh_.param("scale_linear", l_scale_, l_scale_);
+TeleopNode::TeleopNode() {
 
-	vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/arduino/cmd_vel",1);
+    nh.getParam("/teleop_node/scale_angular", a_scale);
+    nh.getParam("/teleop_node/scale_linear", l_scale);
 
-	joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopGarry::joy_cb, this);
+    linear = 1;
+    angular = 2;
 
+    vel_pub = nh.advertise<geometry_msgs::Twist>("/arduino/cmd_vel",1);
+    joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopNode::joy_cb, this);
 }
 
-void TeleopGarry::joy_cb(const sensor_msgs::Joy::ConstPtr& joy) {
-	geometry_msgs::Twist msg;
+void TeleopNode::joy_cb(const sensor_msgs::Joy::ConstPtr& joy) {
 
-	msg.angular.z =joy->axes[angular_];
-	msg.linear.x = joy->axes[linear_];
+    geometry_msgs::Twist msg;
 
-    vel_pub_.publish(msg);
-    usleep(1000);
+    msg.angular.z = a_scale * joy->axes[angular];
+    msg.linear.x = l_scale * joy->axes[linear];
+
+    vel_pub.publish(msg);
 }
 
 int main (int argc, char * argv[]) {
-	ros::init(argc,argv, "teleop_garry");
-	TeleopGarry teleop_garry;
+    ros::init(argc,argv, "teleop_node");
+    TeleopNode teleop_node;
 
-	ros::spin();
+    ros::spin();
 }
