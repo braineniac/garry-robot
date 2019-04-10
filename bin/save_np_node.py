a2ca511015a47e32f0b26c364f104db8f9125f09
Daniel Hammer <braineniac@protonmail.com>
2019-04-10 11:28:50 +0200

    save time, fix wrong topic

--- bin/save_np_node.py
+++ bin/save_np_node.py
@@ -8,25 +8,28 @@ from sensor_msgs.msg import Imu
 class SaveNPNode:
     def __init__(self):
         self.imu_sub = rospy.Subscriber("/imu/data_raw", Imu, self.imu_cb)
-        self.fake_enc_sub = rospy.Subscriber("/fake_wheel/twist_var", TwistWithCovarianceStamped, self.fake_enc_cb)
+        self.fake_enc_sub = rospy.Subscriber("/fake_wheel/twist", TwistWithCovarianceStamped, self.fake_enc_cb)
 
-        self.u_k = np.zeros((1000,3))
+        self.u_k = np.zeros((300,2))
+        self.t_k = np.zeros((300,2))
         self.i = 0
 
     def save_np(self):
         rospy.loginfo("i={}".format(self.i))
-        if self.i == 999:
-            rospy.loginfo("Hello there")
-            np.save("/home/dan/straight.npy",self.u_k)
+        if self.i == 299:
+            rospy.loginfo("Array full")
+            np.save("/home/dan/straight.npy",[self.u_k, self.t_k])
             rospy.signal_shutdown('Quit')
 
     def imu_cb(self, imu_msg):
-        self.u_k[self.i][2] = imu_msg.linear_acceleration.x
+        self.u_k[self.i][1] = imu_msg.linear_acceleration.x
+        self.t_k[self.i][1] = rospy.get_rostime()
         self.save_np()
-        self.i = self.i +1
+        self.i = self.i + 1
 
     def fake_enc_cb(self, twist_msg):
-        self.u_k[self.i][1] = twist_msg.twist.twist.linear.x
+        self.u_k[self.i][0] = twist_msg.twist.twist.linear.x
+        self.t_k[self.i][0] = rospy.get_rostime()
         self.save_np()
         self.i = self.i + 1
 
