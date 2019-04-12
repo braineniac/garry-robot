303bbc6d60acbe405a824498bb45d9c74d326baa
Daniel Hammer <braineniac@protonmail.com>
2019-04-12 10:12:58 +0000

    save after arrays are full

--- bin/save_np_node.py
+++ bin/save_np_node.py
@@ -7,19 +7,24 @@ from sensor_msgs.msg import Imu
 
 class SaveNPNode:
     def __init__(self):
+        self.datapoints=5000
+        self.u_k = np.zeros((self.datapoints,2))
+        self.t_k = np.zeros((self.datapoints,2))
+        self.i = 0
+
         self.imu_sub = rospy.Subscriber("/imu/data_raw", Imu, self.imu_cb)
         self.fake_enc_sub = rospy.Subscriber("/fake_wheel/twist", TwistWithCovarianceStamped, self.fake_enc_cb)
 
-        self.u_k = np.zeros((300,2))
-        self.t_k = np.zeros((300,2))
-        self.i = 0
 
     def save_np(self):
-        rospy.loginfo("i={}".format(self.i))
-        if self.i == 299:
+        np.save("/home/dan/straight_dmp.npy",[self.u_k, self.t_k])
+        rospy.signal_shutdown('Quit')
+
+    def check_full(self):
+        rospy.loginfo("Datapoint {} of 300 saved".format(self.i + 1))
+        if self.i == self.datapoints-1:
             rospy.loginfo("Array full")
-            np.save("/home/dan/straight.npy",[self.u_k, self.t_k])
-            rospy.signal_shutdown('Quit')
+            self.save_np()
 
     def imu_cb(self, imu_msg):
         self.u_k[self.i][1] = imu_msg.linear_acceleration.x
@@ -34,7 +39,8 @@ class SaveNPNode:
         self.i = self.i + 1
 
 if __name__ == '__main__':
-    rospy.loginfo("initialising save_np_node")
+    rospy.loginfo("Initialising save_np_node")
+    rospy.loginfo("Saving up to 300 datapoints")
     rospy.init_node("save_np_node")
     save_np_node = SaveNPNode()
     rate = rospy.Rate(10)
