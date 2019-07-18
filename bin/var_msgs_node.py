c6b3860967abec74cf67c9905c372d442213df9f
Daniel Hammer <braineniac@protonmail.com>
2019-07-18 13:43:56 +0200

    finished var_msgs_node

--- bin/var_msgs_node.py
+++ bin/var_msgs_node.py
@@ -6,7 +6,7 @@ from sensor_msgs.msg import Imu
 import numpy as np
 
 class VarMsgsNode:
-    def __init__(self, imu_in="/imu/filtered",
+    def __init__(self, imu_in="/imu",
                        imu_out="/imu/var",
                        twist_in="/fake_wheel/twist",
                        twist_out="/fake_wheel/twist_var"
@@ -15,17 +15,18 @@ class VarMsgsNode:
         self.twist_pub = rospy.Publisher(twist_out, TwistWithCovarianceStamped, queue_size=1)
 
         self.imu_sub = rospy.Subscriber(imu_in, Imu, self.imu_cb)
-        self.imu_pub = rospy.Subscriber(imu_out, Imu, queue_size=1)
+        self.imu_pub = rospy.Publisher(imu_out, Imu, queue_size=1)
 
         self.twist_peak_vel = 0.14
 
         self.covariance_euler = np.zeros(36)
-        self.covariance_euler[0] = ((400/1000000) * 9.80655)**2 / 100000.0
+        self.covariance_euler[0] = 0.04 / 9
 
     def twist_cb(self, twist_msg):
         new_twist_msg = TwistWithCovarianceStamped()
 
         new_twist_msg.header = twist_msg.header
+        new_twist_msg.header.frame_id = "base_link"
 
         new_twist_msg.twist = twist_msg.twist
 
@@ -37,6 +38,7 @@ class VarMsgsNode:
         new_imu_msg = Imu()
 
         new_imu_msg.header = imu_msg.header
+        new_imu_msg.header.frame_id = "imu_link"
 
         new_imu_msg.orientation = imu_msg.orientation
         new_imu_msg.orientation_covariance = imu_msg.orientation_covariance
@@ -47,7 +49,7 @@ class VarMsgsNode:
         new_imu_msg.linear_acceleration = imu_msg.linear_acceleration
         new_imu_msg.linear_acceleration_covariance = imu_msg.linear_acceleration_covariance
 
-        self.imu_pub(new_imu_msg)
+        self.imu_pub.publish(new_imu_msg)
 
 if __name__ == '__main__':
     rospy.loginfo("Initialising var_msgs_node")
