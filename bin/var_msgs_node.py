f4295c236702c2c0a750f7d6ed2fc62d16a8d760
Daniel Hammer <braineniac@protonmail.com>
2019-08-01 01:10:35 +0200

    new launch files for repeatability

--- bin/var_msgs_node.py
+++ bin/var_msgs_node.py
@@ -9,7 +9,8 @@ class VarMsgsNode:
     def __init__(self, imu_in="/imu",
                        imu_out="/imu/var",
                        twist_in="/fake_wheel/twist",
-                       twist_out="/fake_wheel/twist_var"
+                       twist_out="/fake_wheel/twist_var",
+                       ratio=1/3.
                        ):
         self.twist_sub = rospy.Subscriber(twist_in, TwistWithCovarianceStamped, self.twist_cb)
         self.twist_pub = rospy.Publisher(twist_out, TwistWithCovarianceStamped, queue_size=1)
@@ -17,10 +18,10 @@ class VarMsgsNode:
         self.imu_sub = rospy.Subscriber(imu_in, Imu, self.imu_cb)
         self.imu_pub = rospy.Publisher(imu_out, Imu, queue_size=1)
 
-        self.twist_peak_vel = 0.14
+        self.twist_peak_vel = 0.282087447/2.0
 
         self.covariance_euler = np.zeros(36)
-        self.covariance_euler[0] = 0.04 / 9
+        self.covariance_euler[0] = 0.04 * ratio*ratio
 
     def twist_cb(self, twist_msg):
         new_twist_msg = TwistWithCovarianceStamped()
@@ -54,8 +55,10 @@ class VarMsgsNode:
 if __name__ == '__main__':
     rospy.loginfo("Initialising var_msgs_node")
     rospy.init_node("var_msgs_node")
+    ratio = rospy.get_param("~ratio", 1/3.)
+    rospy.loginfo("The ratio is: {}".format(ratio))
 
-    var_covar_node = VarMsgsNode()
+    var_covar_node = VarMsgsNode(ratio=ratio)
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
