042b5385fb9132056e411aac6cceff58a8028a5a
Daniel Hammer <braineniac@protonmail.com>
2019-08-11 14:37:49 +0000

    granular control in x direction

--- src/teleop_node.cpp
+++ src/teleop_node.cpp
@@ -21,7 +21,10 @@ class TeleopNode {
         void joy_cb(const sensor_msgs::Joy::ConstPtr& joy);
 
         int linear, angular;
-        double l_scale, a_scale;
+        double l_scale=0.0; 
+				double a_scale=0.0;
+				double l_offset=0.0;
+				double a_offset=0.0;
         bool left_eye{false};
         bool right_eye{false};
 
@@ -32,8 +35,10 @@ TeleopNode::TeleopNode() {
 
     nh.getParam("/teleop_node/scale_angular", a_scale);
     nh.getParam("/teleop_node/scale_linear", l_scale);
+		nh.getParam("/teleop_node/l_offset", l_offset);
+		nh.getParam("/teleop_node/a_offset", a_offset);
 
-    linear = 1;
+    linear = 3;
     angular = 2;
 
     vel_pub = nh.advertise<geometry_msgs::Twist>("/arduino/cmd_vel",1);
@@ -46,9 +51,21 @@ void TeleopNode::joy_cb(const sensor_msgs::Joy::ConstPtr& joy) {
 
     geometry_msgs::Twist msg;
 
-    msg.angular.z = a_scale * joy->axes[angular];
-    msg.linear.x = l_scale * joy->axes[linear];
+		if (joy->axes[linear]>0) {
+		
+    msg.linear.x = l_scale * joy->axes[linear] + l_offset;
+		}
+		else if (joy->axes[angular]>0) {
 
+    msg.angular.z = a_scale * joy->axes[angular] + a_offset;
+		}
+		else if (joy->axes[linear]<0) {
+    msg.linear.x = l_scale * joy->axes[linear] + l_offset;
+		}
+		else if (joy->axes[angular]<0) {
+
+		msg.angular.z = a_scale * joy->axes[angular] -  a_offset;
+		}
     // left eye toggle
     if(joy->buttons[1]) {
         std_msgs::Bool msg;
@@ -89,7 +106,7 @@ int main (int argc, char * argv[]) {
 
     ros::init(argc,argv, "teleop_node");
     TeleopNode teleop_node;
-    ros::Rate r(10);
+    ros::Rate r(50);
     while(ros::ok()){
         teleop_node.publish_joy();
            ros::spinOnce();
